2c6492d100c5ed173c5ee509d6599e3c228950b4
Joon Kim <joonkidk@gmail.com>
2022-11-16 11:23:30 -0500

    added bootstrap js and jquery

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-const date = document.querySelector('#date')
+const date = document.querySelector("#date");
+const apiKey = "b169b31281ffa2a2b70b9e8ac22c3e88";
+const todaysDate = moment().format("MMMM Do YYYY");
 
-let todaysDate = moment().format("MMMM Do YYYY")
-
-date.innerText = todaysDate
+date.innerText = todaysDate;
