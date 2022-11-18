7ead99a648b8c6f35ccdb2a06ce6e0422d2939bd
Joon Kim <joonkidk@gmail.com>
2022-11-18 12:30:29 -0500

    finalized code - search history

--- assets/script.js
+++ assets/script.js
@@ -8,28 +8,71 @@ date.innerText = moment().format("MMMM Do YYYY");
 time.innerText = moment().format("h:mm A");
 dayOfWeek.innerText = moment().format("dddd");
 
-input.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    getApi();
-    input.value = "";
-  }
-});
+// applies elements on page load with current position
+function load() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    let fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=b169b31281ffa2a2b70b9e8ac22c3e88&units=imperial`;
+  
+    fetch(fiveDayURL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      fiveDayWeather(data);
+      console.log(data)
+      localStorage.setItem('response', JSON.stringify(data.city.name));
+      loadUrl()
+    })
+  });
+}
 
-function getApi() {
-  let cityName = document.querySelector("#userInput").value;
+function loadUrl() {
+  let cityName = JSON.parse(localStorage.getItem('response'));
 
   let requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b169b31281ffa2a2b70b9e8ac22c3e88&units=imperial`;
-  //   let sunConvert = [];
   fetch(requestURL)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       displayWeather(data);
     });
 }
 
+
+// uses user input as parameter to getApi()
+input.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault()
+    // let cityName = document.querySelectro("#userInput").value;
+    // let li = document.createElement("li")
+    // li.innerText = cityName;
+    // document.querySelector('ul');
+    // ul.appendChild(li);
+    getApi();
+    input.value = "";
+  }
+});
+
+// fetches api using the user input
+function getApi() {
+  let cityName = document.querySelector("#userInput").value;
+
+  let requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b169b31281ffa2a2b70b9e8ac22c3e88&units=imperial`;
+  fetch(requestURL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      // console.log(data);
+      displayWeather(data);
+    });
+}
+
+// uses api data from getApi() and replaces text in html 
 let displayWeather = function (weatherData) {
   document.querySelector("#cityName").innerText = weatherData.name;
   document.querySelector("#temperature").innerText =
@@ -48,18 +91,19 @@ let displayWeather = function (weatherData) {
     weatherData.main.humidity + "%";
   document.querySelector("#pressure").innerText =
     weatherData.main.pressure + " hPa";
-
-  let fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=b169b31281ffa2a2b70b9e8ac22c3e88&units=imperial`;
+    
+    let fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=b169b31281ffa2a2b70b9e8ac22c3e88&units=imperial`;
   fetch(fiveDayURL)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       fiveDayWeather(data);
     });
 };
 
+// obtains lon and lat from previous function then completes new fetch to display 5 day forecast
 let fiveDayWeather = function (weatherValue) {
   let todaysMonth = dayjs().$M;
 
