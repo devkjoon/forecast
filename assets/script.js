0df4956f136d48f17232967111bf600911e238c8
Joon Kim <joonkidk@gmail.com>
2022-11-18 10:02:56 -0500

    added icons to display on fetch + humidity and windspeed status for five day

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,6 @@ const dayOfWeek = document.querySelector("#today");
 
 const input = document.querySelector("#userInput");
 
-const cityName = document.querySelector("#cityName");
-const temperature = document.querySelector("#temperature");
-const humidity = document.querySelector("#humidity");
-const windSpeed = document.querySelector("#windspeed");
-const feelsLike = document.querySelector("#feelslike");
-const tempHigh = document.querySelector("#tempHigh");
-const tempLow = document.querySelector("#tempLow");
-
 date.innerText = moment().format("MMMM Do YYYY");
 time.innerText = moment().format("h:mm A");
 dayOfWeek.innerText = moment().format("dddd");
@@ -39,13 +31,23 @@ function getApi() {
 }
 
 let displayWeather = function (weatherData) {
-  cityName.innerText = weatherData.name;
-  temperature.innerText = Math.floor(weatherData.main.temp) + "\u00B0";
-  humidity.innerText = weatherData.main.humidity + "%";
-  windSpeed.innerText = weatherData.wind.speed + " MPH";
-  feelsLike.innerText = weatherData.main.feels_like + "\u00B0 F";
-  tempHigh.innerText = weatherData.main.temp_max + "\u00B0 F";
-  tempLow.innerText = weatherData.main.temp_min + "\u00B0 F";
+  document.querySelector("#cityName").innerText = weatherData.name;
+  document.querySelector("#temperature").innerText =
+    Math.floor(weatherData.main.temp) + "\u00B0";
+  document.querySelector("#conditions").innerText =
+    weatherData.weather[0].description;
+  document.querySelector("#tempHigh").innerText =
+    weatherData.main.temp_max + "\u00B0 F";
+  document.querySelector("#tempLow").innerText =
+    weatherData.main.temp_min + "\u00B0 F";
+  document.querySelector("#feelslike").innerText =
+    weatherData.main.feels_like + "\u00B0 F";
+  document.querySelector("#windspeed").innerText =
+    weatherData.wind.speed + " MPH";
+  document.querySelector("#humidity").innerText =
+    weatherData.main.humidity + "%";
+  document.querySelector("#pressure").innerText =
+    weatherData.main.pressure + " hPa";
 
   let fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=b169b31281ffa2a2b70b9e8ac22c3e88&units=imperial`;
   fetch(fiveDayURL)
@@ -59,12 +61,23 @@ let displayWeather = function (weatherData) {
 };
 
 let fiveDayWeather = function (weatherValue) {
-
   let todaysMonth = dayjs().$M;
 
-  for( let i = 1; i < 6; i++ ){
-
-      document.querySelector( "#date" + i ).innerText = `${todaysMonth}/${ dayjs().$D + i }`;
-      document.querySelector( "#date" + i + "Temp" ).innerText = weatherValue.list[i].main.temp + "\u00B0 F";
+  for (let i = 1; i < 6; i++) {
+    document.querySelector("#date" + i).innerText = `${todaysMonth}/${
+      dayjs().$D + i
+    }`;
+    document.querySelector("#date" + i + "Temp").innerText =
+      weatherValue.list[i].main.temp + "\u00B0 F";
+    document.querySelector("#condition" + i).innerText =
+      weatherValue.list[i].weather[0].description;
+    document.querySelector("#conditionIcon" + i).src =
+      "http://openweathermap.org/img/wn/" +
+      weatherValue.list[i].weather[0].icon +
+      "@2x.png";
+    document.querySelector("#humidity" + i).innerText =
+      weatherValue.list[i].main.humidity + "%";
+    document.querySelector("#windSpeed" + i).innerText =
+      weatherValue.list[i].wind.speed + "MPH";
   }
-}
+};
